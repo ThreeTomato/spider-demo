d6cba124151981efd3ddf45f6f2eb58793910825
iniestano8 <iniestano8@163.com>
2015-02-01 15:36:48 +0800

    static commit

--- spider-demo/src/com/spider/controller/SiteController.java
+++ spider-demo/src/com/spider/controller/SiteController.java
@@ -1,5 +1,29 @@
 package com.spider.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spider.entity.Site;
+import com.spider.service.ISiteService;
+
+@Controller
+@RequestMapping("/site")
 public class SiteController {
 
+	@Autowired
+	private ISiteService siteService;
+	
+	@RequestMapping("/index")
+	public ModelAndView toSiteIndex(ModelAndView mav){
+		Site site = new Site();
+		List<Site> list = siteService.getList(site);
+		mav.getModel().put("list", list);
+		mav.setViewName("site/site-index");
+		return mav;
+	}
+	
 }
